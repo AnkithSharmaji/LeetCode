a3c4ff036d64439b6096548e083b1693c1b53f02
Ankith Sharma <86291521+AnkithSharmaji@users.noreply.github.com>
2023-07-12 15:30:42 +0530

    Added solution - LeetHub

--- Find kth element of spiral matrix - GFG/find-kth-element-of-spiral-matrix.java
+++ Find kth element of spiral matrix - GFG/find-kth-element-of-spiral-matrix.java
@@ -29,47 +29,46 @@ class Find_Given_Element_Of_Spiral_Matrix
 
 
 class Solution {
-    int findK(int A[][], int n, int m, int k) {
+    int findK(int[][] A, int n, int m, int k) {
         int top = 0, bottom = n - 1, left = 0, right = m - 1;
         int direction = 0; // 0: left to right, 1: top to bottom, 2: right to left, 3: bottom to top
-
-        int count = 0;
+        
         while (top <= bottom && left <= right) {
             if (direction == 0) {
                 for (int i = left; i <= right; i++) {
-                    count++;
-                    if (count == k)
+                    if (--k == 0) {
                         return A[top][i];
+                    }
                 }
                 top++;
                 direction = 1;
             } else if (direction == 1) {
                 for (int i = top; i <= bottom; i++) {
-                    count++;
-                    if (count == k)
+                    if (--k == 0) {
                         return A[i][right];
+                    }
                 }
                 right--;
                 direction = 2;
             } else if (direction == 2) {
                 for (int i = right; i >= left; i--) {
-                    count++;
-                    if (count == k)
+                    if (--k == 0) {
                         return A[bottom][i];
+                    }
                 }
                 bottom--;
                 direction = 3;
             } else if (direction == 3) {
                 for (int i = bottom; i >= top; i--) {
-                    count++;
-                    if (count == k)
+                    if (--k == 0) {
                         return A[i][left];
+                    }
                 }
                 left++;
                 direction = 0;
             }
         }
-
-        return -1; // Return -1 if k is out of range
+        
+        return -1; // Kth element not found
     }
 }
