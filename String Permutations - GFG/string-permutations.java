95627b1885f5ebe46654daf3c4010a3ec55d2350
Ankith Sharma <86291521+AnkithSharmaji@users.noreply.github.com>
2023-08-06 20:20:43 +0530

    Added solution - LeetHub

---
+++ String Permutations - GFG/string-permutations.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int T=sc.nextInt();
+		sc.nextLine();
+		while(T-->0)
+		{
+		    
+		    Solution ob=new Solution();
+		    
+		    String S=sc.nextLine();
+		    
+		    ArrayList<String> arr = ob.permutation(S);
+		    for(String s : arr){
+		        System.out.print(s+" ");
+		    }
+		    System.out.println();
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public ArrayList<String> permutation(String S)
+    {
+        //Your code here
+        ArrayList<String> result = new ArrayList<>();
+        char[] arr = S.toCharArray();
+        permutationHelper(arr, 0, result);
+        Collections.sort(result); // Sort the result in lexicographically increasing order
+        return result;
+    }
+
+    private void permutationHelper(char[] arr, int index, ArrayList<String> result) {
+        if (index == arr.length - 1) {
+            result.add(new String(arr));
+            return;
+        }
+
+        for (int i = index; i < arr.length; i++) {
+            swap(arr, index, i);
+            permutationHelper(arr, index + 1, result);
+            swap(arr, index, i); // Backtrack to restore the original order
+        }
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+	   
+}
