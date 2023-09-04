2e0f5068b1f09b232225ef972a0ac3f10f4c1b82
Ankith Sharma <86291521+AnkithSharmaji@users.noreply.github.com>
2023-09-04 19:53:05 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -60,13 +60,23 @@ class Solution {
     }
     
     static char[][] fill(int n, int m, char a[][]) {
+        // Check top and bottom rows
+        for (int j = 0; j < m; j++) {
+            if (a[0][j] == 'O') {
+                dfs(0, j, n, m, a);
+            }
+            if (a[n - 1][j] == 'O') {
+                dfs(n - 1, j, n, m, a);
+            }
+        }
+        
+        // Check left and right columns
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-                    if (a[i][j] == 'O') {
-                        dfs(i, j, n, m, a);
-                    }
-                }
+            if (a[i][0] == 'O') {
+                dfs(i, 0, n, m, a);
+            }
+            if (a[i][m - 1] == 'O') {
+                dfs(i, m - 1, n, m, a);
             }
         }
         
